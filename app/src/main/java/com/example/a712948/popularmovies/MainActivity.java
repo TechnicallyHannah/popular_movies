f5f0d3fa510aee66efe14bf0f3913d7f506d8d93
Hpaulson <technicalhannah@gmail.com>
2015-09-01 11:53:51 -0600

    more butterknife

--- app/src/main/java/com/example/a712948/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/a712948/popularmovies/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a712948.popularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -9,6 +10,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.inject(this);
         setContentView(R.layout.activity_main);
         getSupportFragmentManager().beginTransaction().add(R.id.container, new MovieFragment()).commit();
     }
